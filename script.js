a044b19f0008f1e0a6c74e4d38fd3d8acf20cb01
thesadgammer22 <130327533+thesadgammert22@users.noreply.github.com>
2024-02-08 12:01:21 -0800

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ ximg.addEventListener("click", function() {
   blur.style.display = "none";
   
 });
+
+let counter = 0;
+ 
+const counterValue = document.getElementById('counter');
+const incrementBtn = document.getElementById('pluscounter');
+const decrementBtn = document.getElementById('minuscounter');
+
+ 
+// To increment the value of counter
+incrementBtn.addEventListener('click', () => {
+    counter++;
+    counterValue.innerHTML = counter;
+});
+ 
+// To decrement the value of counter
+decrementBtn.addEventListener('click', () => {
+    counter--;
+    counterValue.innerHTML = counter;
+});
+ 
