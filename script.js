a9c4c7776c24b5d093fc98eb2848002e6927de6e
thesadgammer22 <130327533+thesadgammert22@users.noreply.github.com>
2024-02-23 14:52:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
- //App.js 
-
-  import ReactGA from 'react-ga';
-  const TRACKING_ID = "UA-XXXXX-X"; // OUR_TRACKING_ID
-  ReactGA.initialize(TRACKING_ID);
-
-var poduct1 = document.getElementById("product1");
-var ximg = document.getElementById("productpanx");
-var panel = document.getElementById("productpan");
-var blur = document.getElementById("blur");
-
-product1.addEventListener("click", function() {
-  panel.style.display = "inline-block";
-  blur.style.display = "inline-block";
-});
-
-
-
-ximg.addEventListener("click", function() {
-  
-  panel.style.display = "none";
-  blur.style.display = "none";
-  
-});
 
 let counter = 0;
  
