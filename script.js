5d26835ae0af940c93ba976bdf85bd3fb91746da
thesadgammer22 <130327533+thesadgammert22@users.noreply.github.com>
2024-02-28 09:26:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,16 @@ var blur = document.getElementById("blur");
 
 product1.addEventListener("click", function() {
   panel.style.display = "inline-block";
+  productpanimg.src = "https://i.ibb.co/J7xmgzB/Screenshot-2024-01-24-9-16-01-AM.png";
   blur.style.display = "inline-block";
+  counter = 1;
+});
+
+product2.addEventListener("click", function() {
+  panel.style.display = "inline-block";
+  productpanimg.src = "https://i.ibb.co/7pbVttN/ezgif-7-07fce48017.gif";
+  blur.style.display = "inline-block";
+  counter = 2;
 });
 
 
