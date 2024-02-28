00a239599fba1891f32ddbd185ce773dd96fe317
thesadgammer22 <130327533+thesadgammert22@users.noreply.github.com>
2024-02-28 08:58:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ let counter = 0;
 const counterValue = document.getElementById('counter');
 const incrementBtn = document.getElementById('pluscounter');
 const decrementBtn = document.getElementById('minuscounter');
+var poduct1 = document.getElementById("product1");
+var ximg = document.getElementById("productpanx");
+var panel = document.getElementById("productpan");
+var blur = document.getElementById("blur");
+
+product1.addEventListener("click", function() {
+  panel.style.display = "inline-block";
+  blur.style.display = "inline-block";
+});
+
+
+
+ximg.addEventListener("click", function() {
+
+  panel.style.display = "none";
+  blur.style.display = "none";
+
+});
 
  
 // To increment the value of counter
