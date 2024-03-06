ea16435ceae7f0b3fef9a61ee80dd71a07cf686c
thesadgammer22 <130327533+thesadgammert22@users.noreply.github.com>
2024-03-06 19:20:37 +0000

    Delete fnfgames directory

--- fnfgames/script.js
+++
@@ -1,9 +0,0 @@
-const htmlCode = document.querySelector(".html");
-const cssCode = document.querySelector(".css");
-const jsCode = document.querySelector(".javascript");
-const display = document.querySelector(".display");
-
-function compileCode () {
-  display.contentDocument.body.innerHTML = htmlCode.value + "<style>" + cssCode.value + "</style>";
-  display.contentWindow.eval(jsCode.value);
-}
