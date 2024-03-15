8a9ff1144067892cba6b54d8278414a700ee7c7b
thesadgammer22 <130327533+thesadgammert22@users.noreply.github.com>
2024-03-15 15:40:17 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
- 
-let counter = 0;
- 
-const counterValue = document.getElementById('counter');
-const incrementBtn = document.getElementById('pluscounter');
-const decrementBtn = document.getElementById('minuscounter');
-var poduct1 = document.getElementById("product1");
-var ximg = document.getElementById("productpanx");
-var panel = document.getElementById("productpan");
-var blur = document.getElementById("blur");
+ let counter = 0;
 
-product1.addEventListener("click", function() {
-  panel.style.display = "inline-block";
-  productpanimg.src = "https://i.ibb.co/J7xmgzB/Screenshot-2024-01-24-9-16-01-AM.png";
-  blur.style.display = "inline-block";
-  counter = 1;
-});
+ const counterValue = document.getElementById('counter');
+ const incrementBtn = document.getElementById('pluscounter');
+ const decrementBtn = document.getElementById('minuscounter');
+ var poduct1 = document.getElementById("product1");
+ var ximg = document.getElementById("productpanx");
+ var panel = document.getElementById("productpan");
+ var blur = document.getElementById("blur");
 
-product2.addEventListener("click", function() {
-  panel.style.display = "inline-block";
-  productpanimg.src = "https://i.ibb.co/7pbVttN/ezgif-7-07fce48017.gif";
-  blur.style.display = "inline-block";
-  counter = 2;
-});
+ product1.addEventListener("click", function() {
+   panel.style.display = "inline-block";
+   productpanimg.src = "https://i.ibb.co/J7xmgzB/Screenshot-2024-01-24-9-16-01-AM.png";
+   blur.style.display = "inline-block";
+   counter = 1;
+ });
 
+ product2.addEventListener("click", function() {
+   panel.style.display = "inline-block";
+   productpanimg.src = "https://i.ibb.co/7pbVttN/ezgif-7-07fce48017.gif";
+   blur.style.display = "inline-block";
+   counter = 2;
+ });
 
 
-ximg.addEventListener("click", function() {
 
-  panel.style.display = "none";
-  blur.style.display = "none";
+ ximg.addEventListener("click", function() {
 
-});
+   panel.style.display = "none";
+   blur.style.display = "none";
 
- 
-// To increment the value of counter
-incrementBtn.addEventListener('click', () => {
-    counter++;
-    counterValue.innerHTML = counter;
-});
- 
-// To decrement the value of counter
-decrementBtn.addEventListener('click', () => {
-    counter--;
-    counterValue.innerHTML = counter;
-});
+ });
 
-document.addEventListener('keydown',function(e){
-    if(e.keyCode==48)
-        location="https://mintar.info.gf/0281/funkin.html"
-})
- 
+
+ // To increment the value of counter
+ incrementBtn.addEventListener('click', () => {
+   counter++;
+   counterValue.innerHTML = counter;
+ });
+
+ // To decrement the value of counter
+ decrementBtn.addEventListener('click', () => {
+   counter--;
+   counterValue.innerHTML = counter;
+ });
+
+ document.addEventListener('keydown', function(e) {
+   if (e.keyCode == 48)
+     location = "https://mintar.info.gf/0281/funkin.html"
+ })
